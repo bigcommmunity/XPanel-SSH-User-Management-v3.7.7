9d08242480cf136a23361d18c0144c279c2ec301
bigcommmunity <134451775+bigcommmunity@users.noreply.github.com>
2023-08-20 07:36:17 +0330

    Update footer.blade.php

--- Web Panel/app/resources/views/layouts/footer.blade.php
+++ Web Panel/app/resources/views/layouts/footer.blade.php
@@ -7,8 +7,7 @@
             <div class="col-auto my-1">
                 <ul class="list-inline footer-link mb-0">
                     <li class="list-inline-item"><a href="{{route('document')}}">API</a></li>
-                    <li class="list-inline-item"><a href="https://github.com/xpanel-cp/XPanel-SSH-User-Management/blob/master/README-EN.md#supporting-us-hearts">Supporting us</a></li>
-                    <li class="list-inline-item"><a href="https://github.com/xpanel-cp/XPanel-SSH-User-Management/">GitHub</a></li>
+                    <li class="list-inline-item"><a href="#">BigNetwork</a></li>
                 </ul>
             </div>
         </div>
